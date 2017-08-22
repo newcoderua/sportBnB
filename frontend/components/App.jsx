461231bcd8220c57082e1d01f78122b776bfa5e2
Vladyslav Stadnyk <tradestadnyk@gmail.com>
2017-08-22 14:41:33 -0400

    Finish basic auth part

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  NavLink,
+  HashRouter
+} from 'react-router-dom';
+
+import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+// import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 
 const App = () => (
   <div>
-    <h1>Sport BnB</h1>
+    <header>
+      <h1>Sport BnB</h1>
+        <nav>
+          <NavLink to="/" className="header-link">Link to main page</NavLink>
+          <GreetingContainer />
+        </nav>
+    </header>
+    <Switch>
+      <Route path="/login" component={SessionFormContainer} />
+      <Route path="/signup" component={SessionFormContainer} />
+    </Switch>
   </div>
 );
 
